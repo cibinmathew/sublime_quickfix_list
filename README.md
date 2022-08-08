da7db22ff2c0b6f07c775de04db290c0449de589
Cibin Mathew <10793628+cibinmathew@users.noreply.github.com>
2022-08-08 21:22:51 +0530

    Sublime quickfix-list

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# sublime-quickfix-list
-Vim quickfix list alternative
+# quickfix_list
+
+Inspired by vim [quickfix and location list](https://freshman.tech/vim-quickfix-and-location-list/), 
+provides an easy way to **navigate back and forth through list of files** populated by **grep**, **fzf**, search results etc
+
+![Alt Text](images/demo.gif)
+
+- `Quickfix List: Next match`: Go to next match
+- `Quickfix List: Open quickfix list file`: Open the custom file
+
+## How to Use
+- Save/open `QUICKFIX_CUSTOM_FILE`. By default it is located at `/tmp/sublime_quickfix_list.txt`
+```
+grep -inH quick "${PWD}"/*.py > /tmp/sublime_quickfix_list.txt
+ ```
+- From Command palette, run `Quickfix List: Next match`
+## Key bindings
+No default keybinding is provided. Use below sample as necessary
+```
+[
+    { "keys": ["ctrl+r"], "command": "quickfix_list" }
+]
+```
+## How to Install
+
+### Install with [Package Control](https://packagecontrol.io/installation)
+todo
+### OR Install manually
+copy this repository into the [Packages](https://docs.sublimetext.io/guide/extensibility/packages.html#package-locations-and-abbreviations) directory
+1. cd into the packages directory. 
+2. clone
+  ```sh
+  $ git clone git@github.com:cibinmathew/sublime_quickfix_list.git
+  ```
+
+### TODOs
+- [ ] Submit to package control channel
+- [ ] Support relative path
+- [ ] Support search results panel
+- [ ] Add FAQ section to readme
+
+
+### Motivation
+
+[VIM quickfix list](https://freshman.tech/vim-quickfix-and-location-list/)
+### Contributors
+
+<a href="https://github.com/cibinmathew/sublime-alter-text/graphs/contributors">
+  <img src="https://contributors-img.web.app/image?repo=cibinmathew/sublime-alter-text" />
+</a>
+
+### License
+
+[MIT License](LICENSE)
---
+++ quickfix_list.py
@@ -0,0 +1,53 @@
+from __future__ import division
+import sublime, sublime_plugin
+import os
+
+# import traceback
+
+
+QuickFix_CurrentLine = -1
+
+# TODO:
+# hotkeys: next, prev, copen, cfirst
+# commands: next, prev, open file
+# support
+# multiple delimiters(:,|)
+# pick vim quickfix file by default or configurable
+# TODO add vim and fzf snippets & even include a gif. Create a Medium.com blog too
+# error handling if match is not valid anymore, if tmp file is not available
+
+settings_path = "QuickfixList.sublime-settings"
+
+
+class QuickfixList(sublime_plugin.TextCommand):
+    def run(self, edit, action="next", next=True):
+        self.settings = sublime.load_settings(settings_path)
+        QUICKFIX_CUSTOM_FILE = self.settings.get(
+            "QUICKFIX_CUSTOM_FILE", "/tmp/sublime_quickfix_list.txt"
+        )
+        global QuickFix_CurrentLine
+
+        if action == "next":
+            for region in self.view.sel():
+                with open(QUICKFIX_CUSTOM_FILE) as f:
+                    txt = f.read()
+                    QuickFix_CurrentLine += 1
+                    QuickFix_CurrentLine = (
+                        0
+                        if QuickFix_CurrentLine >= len(txt.splitlines())
+                        else QuickFix_CurrentLine
+                    )
+                    filename = txt.splitlines()[QuickFix_CurrentLine].strip()
+                    # open_filepaths(self, clean_filepath(filename))
+                    # TODO add try catch
+                    file_path=filename.split(":")[0]
+                    line_num=filename.split(":")[1]
+                    if os.path.isfile(file_path):
+                        print("Opening file_path '%s'" % (file_path))
+                        self.view.window().open_file("{0}:{1}".format(file_path,line_num), sublime.ENCODED_POSITION)
+
+                    else:
+                        print("No filename discovered")
+        elif action == "open":
+            # TODO open at current active match
+            self.view.window().open_file(QUICKFIX_CUSTOM_FILE, sublime.ENCODED_POSITION)
